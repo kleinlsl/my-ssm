557125b9ee0ce8af60d8294c95517b05f14c5a0d
songlinl <songlinl@tujia.com>
2021-09-08 16:49:14 +0800

    add 线程池

---
+++ src/main/java/com/tujia/myssm/executor/EM.java
@@ -0,0 +1,68 @@
+package com.tujia.myssm.executor;
+
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 线程池包装
+ *
+ * @author: songlinl
+ * @create: 2021/09/08 16:16
+ */
+@Slf4j
+public class EM {
+    public static final MonitorExecutorService defaultExecutor;
+
+    static {
+        defaultExecutor = MonitorExecutorService.TraceWrapper.withMonitor(
+                new ThreadPoolExecutor(4, 4, 0L,
+                        TimeUnit.NANOSECONDS, new LinkedBlockingQueue<>(100),
+                        new ThreadFactoryBuilder().setNameFormat("system_default_%d").build())
+        );
+    }
+
+
+    public static void main(String[] args) {
+        final ExecutorService EXECUTOR = EM.defaultExecutor;
+        Map<Integer, Future<List<String>>> futureMap = Maps.newHashMap();
+        for (int i = 0; i < 10; i++) {
+            int finalI = i;
+            Future<List<String>> listFuture = EXECUTOR.submit(() -> {
+                try {
+                    List<String> stringList = Lists.newArrayList();
+                    stringList.add(String.valueOf(finalI));
+                    if (finalI == 5) {
+                        throw new RuntimeException("出异常了");
+                    }
+                    return stringList;
+                } catch (Exception e) {
+                    // TODO: 2021/9/8 异常怎么返回空的Future
+                    log.error("[WxAccountDrainageBiz.queryMemberReceived] 异步查卷异常", e);
+                    return null;
+                }
+            });
+            futureMap.put(i, listFuture);
+        }
+        for (int i = 0; i < 10; i++) {
+            try {
+                List<String> stringList = futureMap.get(i).get(10, TimeUnit.MILLISECONDS);
+                System.out.println("stringList = " + stringList);
+            } catch (Exception e) {
+                log.error("异常:{}", i, e);
+            } finally {
+                futureMap.get(i).cancel(true);
+            }
+        }
+
+    }
+}
---
+++ src/main/java/com/tujia/myssm/executor/MonitorExecutorService.java
@@ -0,0 +1,160 @@
+package com.tujia.myssm.executor;
+
+import com.google.common.base.Preconditions;
+import com.tujia.common.focus.TraceHelper;
+import org.apache.commons.collections.CollectionUtils;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * 扩展 jdk ExecutorService
+ *
+ * @author: songlinl
+ * @create: 2021/09/08 16:00
+ */
+public interface MonitorExecutorService extends ExecutorService {
+
+    /**
+     * core thread num
+     */
+    int getCorePoolSize();
+
+    /**
+     * max thread num
+     */
+    int getLargestPoolSize();
+
+    /**
+     * remaining task num in the queue
+     *
+     * @return
+     */
+    int getQueueSize();
+
+    /**
+     * the approximate number of threads that are actively executing tasks.
+     *
+     * @return
+     */
+    int getActiveCount();
+
+    public static final class TraceWrapper {
+        static MonitorExecutorService withMonitor(ThreadPoolExecutor e) {
+            Preconditions.checkNotNull(e);
+            ExecutorService tWrappedExecutor = TraceHelper.wrap(e);
+            return new MonitorExecutorService() {
+                @Override
+                public int getCorePoolSize() {
+                    return e.getCorePoolSize();
+                }
+
+                @Override
+                public int getLargestPoolSize() {
+                    return e.getLargestPoolSize();
+                }
+
+                @Override
+                public int getQueueSize() {
+                    return e.getQueue().size();
+                }
+
+                @Override
+                public int getActiveCount() {
+                    return e.getActiveCount();
+                }
+
+                @Override
+                public void execute(Runnable command) {
+                    tWrappedExecutor.execute(command);
+                }
+
+                @Override
+                public void shutdown() {
+                    tWrappedExecutor.shutdown();
+                }
+
+                @Override
+                public List<Runnable> shutdownNow() {
+                    return tWrappedExecutor.shutdownNow();
+                }
+
+                @Override
+                public boolean isShutdown() {
+                    return tWrappedExecutor.isShutdown();
+                }
+
+                @Override
+                public boolean isTerminated() {
+                    return tWrappedExecutor.isTerminated();
+                }
+
+                @Override
+                public boolean awaitTermination(long timeout, TimeUnit unit) throws InterruptedException {
+                    return tWrappedExecutor.awaitTermination(timeout, unit);
+                }
+
+                @Override
+                public <T> Future<T> submit(Callable<T> task) {
+                    return tWrappedExecutor.submit(task);
+                }
+
+                @Override
+                public <T> Future<T> submit(Runnable task, T result) {
+                    return tWrappedExecutor.submit(task, result);
+                }
+
+                @Override
+                public Future<?> submit(Runnable task) {
+                    return tWrappedExecutor.submit(task);
+                }
+
+                @Override
+                public <T> List<Future<T>> invokeAll(Collection<? extends Callable<T>> tasks)
+                        throws InterruptedException {
+                    if (CollectionUtils.isEmpty(tasks)) {
+                        return Collections.emptyList();
+                    }
+                    return tWrappedExecutor.invokeAll(tasks);
+                }
+
+                @Override
+                public <T> List<Future<T>> invokeAll(Collection<? extends Callable<T>> tasks, long timeout,
+                                                     TimeUnit unit) throws InterruptedException {
+                    if (CollectionUtils.isEmpty(tasks)) {
+                        return Collections.emptyList();
+                    }
+                    return tWrappedExecutor.invokeAll(tasks, timeout, unit);
+                }
+
+                @Override
+                public <T> T invokeAny(Collection<? extends Callable<T>> tasks)
+                        throws InterruptedException, ExecutionException {
+                    if (CollectionUtils.isEmpty(tasks)) {
+                        return null;
+                    }
+                    return tWrappedExecutor.invokeAny(tasks);
+                }
+
+                @Override
+                public <T> T invokeAny(Collection<? extends Callable<T>> tasks, long timeout, TimeUnit unit)
+                        throws InterruptedException, ExecutionException, TimeoutException {
+                    if (CollectionUtils.isEmpty(tasks)) {
+                        return null;
+                    }
+                    return tWrappedExecutor.invokeAny(tasks, timeout, unit);
+                }
+            };
+        }
+    }
+
+
+}
